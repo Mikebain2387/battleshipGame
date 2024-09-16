1644ab6bf6b55b91c451dcba5db552d34c7cf984
Michael Bain <150988106+Mikebain2387@users.noreply.github.com>
2024-09-16 13:12:10 -0500

    update js add conffetti

--- app.js
+++ app.js
@@ -13,6 +13,41 @@ for (let i = 0; i < guess.length; i++) {
          // add class to change color if hit or miss
             guess[i].classList.add('ifHit');
             message.textContent = 'You Sank My Battleship';
+
+            const duration = 3 * 1000,
+            animationEnd = Date.now() + duration,
+            defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+          
+          function randomInRange(min, max) {
+            return Math.random() * (max - min) + min;
+          }
+          
+          const interval = setInterval(function() {
+            const timeLeft = animationEnd - Date.now();
+          
+            if (timeLeft <= 0) {
+              return clearInterval(interval);
+            }
+          
+            const particleCount = 50 * (timeLeft / duration);
+          
+            // since particles fall down, start a bit higher than random
+            confetti(
+              Object.assign({}, defaults, {
+                particleCount,
+                origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+              })
+            );
+            confetti(
+              Object.assign({}, defaults, {
+                particleCount,
+                origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+              })
+            );
+          }, 100);
+
+
+
         } else {
             guess[i].classList.add('ifMiss');
             message.textContent = 'You Missed, Try Again';
